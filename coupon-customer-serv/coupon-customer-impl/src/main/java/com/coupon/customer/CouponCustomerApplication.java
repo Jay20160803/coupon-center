039d00b3ff6236a11e830aa8be2390e31ff270ae
472781184@qq.com <Abc258258>
2022-04-24 17:19:16 +0800

    coupon_customer

---
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/coupon/customer/CouponCustomerApplication.java
@@ -0,0 +1,25 @@
+package com.coupon.customer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan(basePackages = {"com.coupon"})
+@EnableTransactionManagement
+//用于扫描Dao @Repository
+@EnableJpaRepositories(basePackages = {"com.coupon"})
+//用于扫描JPA实体类 @Entity，默认扫本包当下路径
+@EntityScan(basePackages = {"com.coupon"})
+public class CouponCustomerApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(CouponCustomerApplication.class,args);
+    }
+}
---
+++ coupon-customer-serv/coupon-customer-impl/src/main/java/com/coupon/customer/config/JpaConfiguration.java
@@ -0,0 +1,7 @@
+package com.coupon.customer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+public class JpaConfiguration {
+}
